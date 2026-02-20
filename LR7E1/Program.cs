aa21225ec4cba7e5baac0535b73c63d161307f58
dmitrykorolev-work <dmitrykorolev.work@gmail.com>
2026-02-20 23:59:41 +0200

    Initial commit

---
+++ LR7E1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace LR7E1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        List<Action> actions = new();
+
+        for (int i = 1; i <= 5; i++)
+        {
+            //actions.Add( () => Console.WriteLine(i) );
+
+            int localI = i; // Capture the current value of i
+            actions.Add( () => Console.WriteLine(localI) ); // Use the local variable in the lambda expression
+        }
+
+        foreach (var action in actions)
+        {
+            action();
+        }
+    }
+}
