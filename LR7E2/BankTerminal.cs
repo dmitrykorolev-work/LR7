0c5cac6336aa52803659a01410bbdcb3e0715953
dmitrykorolev-work <dmitrykorolev.work@gmail.com>
2026-02-21 00:01:27 +0200

    Added part 2

---
+++ LR7E2/BankTerminal.cs
@@ -0,0 +1,13 @@
+﻿namespace LR7E2;
+
+internal class BankTerminal
+{
+    //internal Action<int> OnMoneyWithdraw;
+    internal event Action<int> OnMoneyWithdraw;
+
+    public void Withdraw(int amount)
+    {
+        Console.WriteLine($"Withdrawing {amount}");
+        OnMoneyWithdraw?.Invoke(amount);
+    }
+}
---
+++ LR7E2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace LR7E2;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var terminal = new BankTerminal();
+        terminal.OnMoneyWithdraw += amount => Console.WriteLine($"You have withdrawn {amount}.");
+
+
+        terminal.Withdraw(100); // Output: You have withdrawn 100.
+
+        /*
+        // The event 'BankTerminal.OnMoneyWithdraw' can only appear on the left hand side of += or -= (except when used from within the type 'BankTerminal')
+        terminal.OnMoneyWithdraw?.Invoke(1000000); // Triggering an event manually without an operation
+        terminal.OnMoneyWithdraw = null; // Unsubscribe all handlers
+        */
+
+        terminal.Withdraw(200);
+
+
+    }
+}
